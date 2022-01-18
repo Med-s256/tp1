3fef8bf8c3e1d7607580ff43d7bd0f6a41116c25
meguebli <ali.meguebli@gmail.com>
2022-01-18 14:13:00 +0100

    modification class Principale

--- Principale.java
+++ Principale.java
@@ -3,7 +3,7 @@ public class Principale{
         System.out.println(" voici je suis dans la classe principale");
     }
 
-    public static void main(Sting []args){
+    public static void main(String []args){
         Animal a =new Animal();
         Personne p =new Personne();
         Chat c =new Chat();
