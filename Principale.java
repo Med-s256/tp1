a43e8621fd6b9463cecbe23b2c10ac3e4dd4d7e3
meguebli <ali.meguebli@gmail.com>
2022-01-18 13:55:10 +0100

    Ajouter classe Principale

---
+++ Principale.java
@@ -0,0 +1,13 @@
+public class Principale{
+    Principale(){
+        System.out.println(" voici je suis dans la classe principale");
+    }
+
+    public static void main(Sting []args){
+        Animal a =new Animal();
+        Personne p =new Personne();
+        Chat c =new Chat();
+
+
+    }
+}
