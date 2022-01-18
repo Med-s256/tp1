3ac9f58974bd9572d5086c9ac782967a6f0694c9
Hicham Mounadi <hichammounadi@users.noreply.github.com>
2022-01-18 13:54:50 +0100

    adding Animal class

---
+++ animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    String name;
+    int nombrePied;
+    
+    public Animal (String name,int nombrePied) {
+        this.name = name;
+	this.nombeePied = nombrePied;
+    }
+}
